20a444e01f1c550f03eba28c48bdcf51cecd33b0
이현근 <leehyunk6310@naver.com>
2023-09-11 18:49:53 +0900

    Feat: 사용자 관련 세팅

---
+++ backend/giggyapp/src/main/java/com/d208/giggyapp/dto/SignUpDto.java
@@ -0,0 +1,14 @@
+package com.d208.giggyapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignUpDto {
+    private String token;
+}
