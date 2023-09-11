8252ebd4ab87a3c059642f74ac09ebfe09fa9a3a
이현근 <leehyunk6310@naver.com>
2023-09-11 14:34:21 +0900

    Chore: 프로젝트 초기화

---
+++ giggyapp/src/main/java/com/d208/giggyapp/GiggyappApplication.java
@@ -0,0 +1,13 @@
+package com.d208.giggyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiggyappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiggyappApplication.class, args);
+	}
+
+}
