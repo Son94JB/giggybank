9cce646588e07e4e18a0398cb324e9af698447c1
김준성 <know0329@pusan.ac.kr>
2023-09-12 17:09:46 +0900

    Feat: 홈 화면 UI 구성 완료, animation 완료

--- mobile/app/src/main/java/com/d208/giggybank/ui/MainActivity.kt
+++ mobile/app/src/main/java/com/d208/giggybank/ui/MainActivity.kt
@@ -1,24 +1,22 @@
-package com.d208.giggybank
+package com.d208.giggybank.ui
 
 import android.content.pm.ActivityInfo
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.navigation.fragment.NavHostFragment
 import com.d103.asaf.common.config.BaseActivity
+import com.d208.giggybank.R
 import com.d208.giggybank.databinding.ActivityMainBinding
-import com.kakao.sdk.common.KakaoSdk
 import com.kakao.sdk.common.util.Utility
 import dagger.hilt.android.AndroidEntryPoint
 
-private const val TAG = "MainActivity giggy"
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val keyHash = Utility.getKeyHash(this)
-        Log.e("Key","keyHash: $keyHash")
+        Log.e("Key", "keyHash: $keyHash")
 //        /** KakaoSDK init */
 ////        KakaoSdk.init(this, this.getString(R.string.kakao_app_key))
 
---
+++ mobile/app/src/main/java/com/d208/giggybank/ui/home/HomeFragment.kt
@@ -0,0 +1,148 @@
+package com.d208.giggybank.ui.home
+
+import android.animation.ObjectAnimator
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import com.d103.asaf.common.config.BaseFragment
+import com.d208.giggybank.R
+import com.d208.giggybank.databinding.FragmentHomeBinding
+import com.d208.giggybank.ui.MainActivityVIewModel
+import com.daimajia.androidanimations.library.Techniques
+import com.daimajia.androidanimations.library.YoYo
+import dagger.hilt.android.AndroidEntryPoint
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+@AndroidEntryPoint
+class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::bind, R.layout.fragment_home) {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+    private val homeFragmentViewModel : HomeFragmentViewModel by viewModels()
+    private val mainActivityViewModel : MainActivityVIewModel by activityViewModels()
+    private var amountPercent = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        test()
+        animate()
+        init()
+    }
+    fun test()
+    {
+        amountPercent = 30
+    }
+    fun animate() {
+
+        with(binding){
+            YoYo.with(Techniques.ZoomIn)
+                .duration(1000)
+                .repeat(0)
+                .playOn(fragmentHomeConsumptionAnalysisCardView)
+
+            YoYo.with(Techniques.ZoomIn)
+                .duration(1000)
+                .repeat(0)
+                .playOn(fragmentHomeCommunityCardView)
+
+            YoYo.with(Techniques.ZoomIn)
+                .duration(1000)
+                .repeat(0)
+                .playOn(fragmentHomeMiniGameCardView)
+
+            YoYo.with(Techniques.ZoomIn)
+                .duration(1000)
+                .repeat(0)
+                .playOn(fragmentHomeSettingCardView)
+
+            YoYo.with(Techniques.BounceIn)
+                .duration(1500)
+                .repeat(0)
+                .playOn(cardView)
+
+
+        }
+
+
+
+    }
+
+    fun init(){
+        with(binding){
+
+
+            //애니메이션
+
+
+
+            //닉네임
+            fragmentHomeNickNameTextView.text = "${mainActivityViewModel.user.nickname} 님"
+
+
+            //은행 정보
+
+
+            // 소비 패턴 분석 화면
+            fragmentHomeConsumptionAnalysisCardView.setOnClickListener {
+
+            }
+            // 미니 게임 화면
+            fragmentHomeMiniGameCardView.setOnClickListener {
+
+            }
+            // 커뮤니티 화면
+            fragmentHomeCommunityCardView.setOnClickListener {
+
+            }
+            // 설정 화면
+            fragmentHomeSettingCardView.setOnClickListener {
+
+            }
+
+            ObjectAnimator.ofInt(fragmenthomeProgressBar, "progress", amountPercent.toInt())
+                .setDuration(500)
+                .start()
+        }
+
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment HomeFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            HomeFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
