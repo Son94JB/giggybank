009c2243f3ca118a27b151552a741c07be35b4d9
이현근 <leehyunk6310@naver.com>
2023-09-19 10:30:50 +0900

    Feat: 토큰 필터 및 재발급 기능 추가

--- backend/giggyapp/src/main/java/com/d208/giggyapp/service/UserService.java
+++ backend/giggyapp/src/main/java/com/d208/giggyapp/service/UserService.java
@@ -1,17 +1,17 @@
 package com.d208.giggyapp.service;
 
 import com.d208.giggyapp.domain.User;
-import com.d208.giggyapp.dto.User.KakaoResponseDto;
-import com.d208.giggyapp.dto.User.LoginDto;
-import com.d208.giggyapp.dto.User.UserDto;
+import com.d208.giggyapp.dto.User.*;
 import com.d208.giggyapp.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,6 +19,8 @@ import org.springframework.web.client.RestTemplate;
 @Transactional
 @RequiredArgsConstructor
 public class UserService {
+    private final RedisService redisService;
+
     private final UserRepository userRepository;
 
     // 토큰 만료기간 확인
@@ -83,7 +85,41 @@ public class UserService {
                 fcmToken(user.getFcmToken()).
                 refreshToken(user.getRefreshToken()).
                 leftLife(user.getLeftLife()).build();
+
         return userDto;
+    }
+
+    // 리프레시 토큰을 조회해서 새로 액세스 토큰 발급하기
+    public String issueAccessToken(String refreshToken) {
+
+        // 리프레시 토큰으로 카카오 서버와 연동해서 accessToken 발급받기
+        String url = "https://kauth.kakao.com/oauth/token";
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", "application/x-www-form-urlencoded;");
+
+        // 요청을 보낼 데이터 생성
+        MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
+        requestBody.add("grant_type", "refresh_token");
+        requestBody.add("client_id", "3d8e6aac6079ec287090de67e9d97584"); // 환경 변수로 빼야함
+        requestBody.add("refresh_token", refreshToken);
+
+        // 요청 엔티티 생성
+        HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<>(requestBody, headers);
+        ResponseEntity<KakaoRefreshResponseDto> response = restTemplate.exchange(url, HttpMethod.POST, requestEntity, KakaoRefreshResponseDto.class);
+        KakaoRefreshResponseDto kakaoRefreshResponseDto = response.getBody();
+        String accessToken = kakaoRefreshResponseDto.getAccessToken();
+
+        // 레디스에 토큰 저장
+        redisService.setAccessToken(TokenDto.builder()
+                .accessToken(accessToken)
+                .exist(1)
+                .build());
+
+
+        // 클라이언트로 내려주기
+        return accessToken;
     }
 }
 
