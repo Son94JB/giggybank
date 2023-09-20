e7f77a4c749d6380c56c7e4b229a77f8bcdbcf36
손준배 <thswnsqo99@gmail.com>
2023-09-20 21:48:08 +0900

    testing jenkins

--- backend/giggyrank/src/main/java/com/d208/giggyrank/GiggyrankApplication.java
+++ backend/giggyrank/src/main/java/com/d208/giggyrank/GiggyrankApplication.java
@@ -2,8 +2,10 @@ package com.d208.giggyrank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class GiggyrankApplication {
 
 	public static void main(String[] args) {
