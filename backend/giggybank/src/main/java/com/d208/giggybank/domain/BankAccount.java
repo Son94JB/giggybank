ada069870b1413e0ef3bd04aa3818273c1c51e09
SonJunBae <thswnsqo99@gmail.com>
2023-09-21 15:31:39 +0900

    no message

--- backend/giggybank/src/main/java/com/d208/giggybank/domain/BankAccount.java
+++ backend/giggybank/src/main/java/com/d208/giggybank/domain/BankAccount.java
@@ -21,7 +21,7 @@ public class BankAccount {
     @JoinColumn(name="CUSTOMER_ID")
     private Customer customer;
 
-    @OneToMany(mappedBy = "bankAccount", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "bankAccount", fetch = FetchType.LAZY)
     private List<BankAccountHistory> bankAccountHistories = new ArrayList<>();
 
 
