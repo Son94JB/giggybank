30f4fe3836700cdd6e187eca5a35fd15c920003d
이현근 <leehyunk6310@naver.com>
2023-09-21 16:42:20 +0900

    Feat: 1원 입금시 모바일로 거래내역 전달

--- backend/giggyapp/src/main/java/com/d208/giggyapp/controller/BankController.java
+++ backend/giggyapp/src/main/java/com/d208/giggyapp/controller/BankController.java
@@ -1,11 +1,12 @@
 package com.d208.giggyapp.controller;
 
-import com.d208.giggyapp.domain.User;
-import com.d208.giggyapp.dto.Auth1Dto;
+import com.d208.giggyapp.dto.Auth1RequestDto;
+import com.d208.giggyapp.dto.Auth1ResponseDto;
 import com.d208.giggyapp.repository.UserRepository;
 import com.d208.giggyapp.service.BankService;
 import com.d208.giggyapp.service.FirebaseCloudMessageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,18 +22,28 @@ public class BankController {
     private final FirebaseCloudMessageService firebaseCloudMessageService;
 
     private final UserRepository userRepository;
+
     // 1원 인증
     @PostMapping("/bank/auth")
-    public void oneAuth(@RequestBody Auth1Dto auth1Dto) throws IOException {
+    public ResponseEntity<Auth1MobileDto> oneAuth(@RequestBody Auth1RequestDto auth1RequestDto) throws IOException {
         // 은행에 1원 인증 요청해서 응답을 받는다.
-        bankService.oneAuthRequest(auth1Dto);
+        Auth1ResponseDto auth1ResponseDto = bankService.oneAuthRequest(auth1RequestDto);
+
+        if (auth1ResponseDto.getContent() == null) return ResponseEntity.ok(Auth1MobileDto.builder()
+                .type(1)
+                .content("")
+                .build());
 
         // FCM
         // 서비스로 분리해야함.
-        String token = "";
-        String title = "1원 송금";
-        String body = "나 무 늘 보";
+        String token = auth1RequestDto.getFcmToken();
+        String title = "1 원 입 금";
+        String body = auth1ResponseDto.getContent();
         firebaseCloudMessageService.sendMessageTo(token, title, body);
 
+        return ResponseEntity.ok(Auth1MobileDto.builder()
+                .type(2)
+                .content(body)
+                .build());
     }
 }
