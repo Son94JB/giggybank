73106ffd4962c18ad77cd99d62c93c77ac483aaf
jaewook <jaewook71828@gmail.com>
2023-09-22 10:00:37 +0900

    거지랭킹 수정중

--- backend/giggyrank/src/main/java/com/d208/giggyrank/dto/BeggerRankDto.java
+++ backend/giggyrank/src/main/java/com/d208/giggyrank/dto/BeggerRankDto.java
@@ -1,4 +1,12 @@
 package com.d208.giggyrank.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class BeggerRankDto {
+    private String userName;
+    private int targetAmount;
+    private int currentAmount;
 }
