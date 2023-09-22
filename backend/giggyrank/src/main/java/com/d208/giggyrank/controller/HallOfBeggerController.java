5bdaee757868c93f97404ebd999a03ae3df8523a
jaewook <jaewook71828@gmail.com>
2023-09-22 10:03:46 +0900

    Revert "거지랭킹 수정중"
    This reverts commit c4f921349583a7e77c550f28a6c0273175b9054c.

--- backend/giggyrank/src/main/java/com/d208/giggyrank/controller/HallOfBeggerController.java
+++ backend/giggyrank/src/main/java/com/d208/giggyrank/controller/HallOfBeggerController.java
@@ -1,16 +1,4 @@
 package com.d208.giggyrank.controller;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Required;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Controller;
-
-@Controller
-@RequiredArgsConstructor
 public class HallOfBeggerController {
-    @Autowired
-    public final RedisTemplate<String, String> redisTemplate;
-
-    
 }
--- backend/giggyrank/src/main/java/com/d208/giggyrank/dto/BeggerRankDto.java
+++ backend/giggyrank/src/main/java/com/d208/giggyrank/dto/BeggerRankDto.java
@@ -1,12 +1,4 @@
 package com.d208.giggyrank.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class BeggerRankDto {
-    private String userName;
-    private int targetAmount;
-    private int currentAmount;
 }
