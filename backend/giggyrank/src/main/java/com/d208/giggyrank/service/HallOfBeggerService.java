2c1f29156ccc6465e232f46cecf87398ec810204
jaewook <jaewook71828@gmail.com>
2023-09-22 10:03:46 +0900

    Revert "거지랭킹 수정중"
    This reverts commit c4f921349583a7e77c550f28a6c0273175b9054c.

--- backend/giggyrank/src/main/java/com/d208/giggyrank/service/HallOfBeggerService.java
+++ backend/giggyrank/src/main/java/com/d208/giggyrank/service/HallOfBeggerService.java
@@ -1,24 +1,4 @@
 package com.d208.giggyrank.service;
 
-import com.d208.giggyrank.dto.BeggerRankDto;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
-
-@Service
 public class HallOfBeggerService {
-    private static final String RANK_KEY = "beggerRank";
-
-    @Autowired
-    private RedisTemplate<String, String> redisTemplate;
-
-    @Transactional
-    public void update(BeggerRankDto beggerRankDto){
-        Integer targetAmount = beggerRankDto.getTargetAmount();
-        Integer currentAmount = beggerRankDto.getCurrentAmount();
-        Double consumptionRatio = (double) (currentAmount/targetAmount);
-        redisTemplate.opsForZSet().add(RANK_KEY, beggerRankDto.getUserName(), consumptionRatio);
-    }
 }
