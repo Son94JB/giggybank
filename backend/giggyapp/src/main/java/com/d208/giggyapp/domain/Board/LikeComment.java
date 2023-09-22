a56e55e3764b71a5996640742fdfcf40afcee54a
김현지 <hjkasd12345@naver.com>
2023-09-22 17:25:08 +0900

    Feat: 게시판 엔티티 생성

---
+++ backend/giggyapp/src/main/java/com/d208/giggyapp/domain/Board/LikeComment.java
@@ -0,0 +1,29 @@
+package com.d208.giggyapp.domain.Board;
+
+import com.d208.giggyapp.domain.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LikeComment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="USER_ID")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "COMMENT_ID")
+    private Comment comment;
+
+}
