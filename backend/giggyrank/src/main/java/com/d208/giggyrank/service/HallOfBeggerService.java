92849b6911474413d839d6018035ed167ea09c3f
SonJunBae <thswnsqo99@gmail.com>
2023-09-22 16:51:58 +0900

    Fix: 거지 랭킹 Domain생성

--- backend/giggyrank/src/main/java/com/d208/giggyrank/service/HallOfBeggerService.java
+++ backend/giggyrank/src/main/java/com/d208/giggyrank/service/HallOfBeggerService.java
@@ -1,4 +1,12 @@
 package com.d208.giggyrank.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class HallOfBeggerService {
 }
