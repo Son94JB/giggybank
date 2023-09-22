5c04f96b1487da8ce11e14c4c20f6a87c3004d63
jaewook <jaewook71828@gmail.com>
2023-09-22 09:58:08 +0900

    Feat : 분석서버에서 은행거래내역 받아오고 저장 구현

--- backend/giggyapp/src/main/java/com/d208/giggyapp/repository/AppAccountHistoryRepository.java
+++ backend/giggyapp/src/main/java/com/d208/giggyapp/repository/AppAccountHistoryRepository.java
@@ -1,10 +1,14 @@
 package com.d208.giggyapp.repository;
 
 import com.d208.giggyapp.domain.AppAccountHistory;
+import com.d208.giggyapp.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 
 @Repository
 public interface AppAccountHistoryRepository extends JpaRepository<AppAccountHistory, Long> {
+    Optional<AppAccountHistory> findFirstByUserOrderByTransactionDate(User user);
 }
