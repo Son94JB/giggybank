c4f921349583a7e77c550f28a6c0273175b9054c
jaewook <jaewook71828@gmail.com>
2023-09-22 10:00:37 +0900

    거지랭킹 수정중

--- backend/giggyrank/src/main/java/com/d208/giggyrank/repository/HallOfBeggerRepository.java
+++ backend/giggyrank/src/main/java/com/d208/giggyrank/repository/HallOfBeggerRepository.java
@@ -1,9 +1,11 @@
 package com.d208.giggyrank.repository;
 
 import com.d208.giggyrank.domain.begger.HallOfBegger;
+import com.d208.giggyrank.dto.BeggerRankDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface HallOfBeggerRepository extends JpaRepository<HallOfBegger, Long> {
+public interface HallOfBeggerRepository extends CrudRepository<BeggerRankDto, String> {
 }
