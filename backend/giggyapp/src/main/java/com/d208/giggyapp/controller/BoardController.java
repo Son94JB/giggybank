f7d23d99d5e0e52a176b4a7527c3ceadb541d6ab
김현지 <hjkasd12345@naver.com>
2023-09-22 17:25:08 +0900

    Feat: 게시판 엔티티 생성

---
+++ backend/giggyapp/src/main/java/com/d208/giggyapp/controller/BoardController.java
@@ -0,0 +1,16 @@
+package com.d208.giggyapp.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/board")
+public class BoardController {
+
+
+
+}
