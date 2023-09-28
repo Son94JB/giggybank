fba498d7f0c2f8144ad38b810cf42a0e731deff9
김현지 <hjkasd12345@naver.com>
2023-09-29 03:21:52 +0900

    Feat: postType별 게시글 조회 하는 중

--- backend/giggyapp/src/main/java/com/d208/giggyapp/repository/board/PostRepository.java
+++ backend/giggyapp/src/main/java/com/d208/giggyapp/repository/board/PostRepository.java
@@ -1,6 +1,7 @@
 package com.d208.giggyapp.repository.board;
 
 import com.d208.giggyapp.domain.board.Post;
+import com.d208.giggyapp.domain.board.PostType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -15,4 +16,21 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     void increaseViewCnt(@Param("id") Long id);
 
     List<Post> findAllByTitleContainingIgnoreCaseOrderByIdDesc(String keyword);
+
+    default List<Post> findAllByTitleAndpostType(String keyword, PostType postType) {
+        if (postType == null) {
+            return findAllByTitleContainingIgnoreCaseOrderByIdDesc(keyword);
+        } else {
+            return findAllByTitleContainingIgnoreCaseAndpostTypeOrderByIdDesc(keyword, postType);
+        }
+    }
+
+
+    @Query(value = "select post from Post post where post.title LIKE  '%:keyword%' " +
+            "AND post.postType = :postType ")
+    List<Post> findAllByTitleContainingIgnoreCaseAndpostTypeOrderByIdDesc(@Param("keyword") String keyword, @Param("postType") PostType postType);
+
+//    @Query(value = "select appAccountHistory from AppAccountHistory appAccountHistory " +
+//            "where appAccountHistory.user = :user AND " +
+//            "appAccountHistory.transactionDate BETWEEN :startDate AND :endDate")
 }
