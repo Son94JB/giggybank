d0e9dc66790cc046fd5741f94fca5e6fe339343d
손준배 <thswnsqo99@gmail.com>
2023-10-02 01:16:03 +0900

    Feat: Redis에 점수 저장 및 랭킹 기능 구현 중

--- backend/giggyrank/src/main/java/com/d208/giggyrank/repository/GameRankRepository.java
+++ backend/giggyrank/src/main/java/com/d208/giggyrank/repository/GameRankRepository.java
@@ -2,8 +2,13 @@ package com.d208.giggyrank.repository;
 
 import com.d208.giggyrank.domain.game.GameRank;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
-public interface GameRankRepository extends CrudRepository<GameRank, UUID> {
+@Repository
+public interface GameRankRepository extends CrudRepository<GameRank, Long> {
+    Optional<GameRank> findByUserId(UUID userId);
+
 }
--- backend/giggyrank/src/main/java/com/d208/giggyrank/service/GameRankService.java
+++ backend/giggyrank/src/main/java/com/d208/giggyrank/service/GameRankService.java
@@ -4,6 +4,8 @@ import com.d208.giggyrank.domain.game.GameRank;
 import com.d208.giggyrank.dto.GameRankDto;
 import com.d208.giggyrank.repository.GameRankRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -20,10 +22,12 @@ public class GameRankService {
     // Redis 서버에 각 유저의 점수를 저장하는 로직
     // 점수를 불러올 때 해당 유저의 현재 저장된 있는 점수와 비교해서 새로운 점수가 더 크다면 바꿔서 저장
     // 아니라면 그냥 스킵
-    public Boolean saveScore(GameRankDto gameRankDto) {
+    public ResponseEntity<String> saveScore(GameRankDto gameRankDto) {
 
         UUID userId = gameRankDto.getUserId();
-        int newScore = gameRankDto.getScore();
+        Integer newScore = gameRankDto.getScore();
+        System.out.println(userId);
+        System.out.println(newScore);
 
         // 일단 라운드 계산을 해준다
         Long today = LocalDateTime.now().atZone(ZoneId.of("Asia/Seoul")).toInstant().toEpochMilli();
@@ -38,22 +42,23 @@ public class GameRankService {
 
         // 게임 랭킹에 기존 랭킹이 있는지 확인
         // 없으면 바로 저장해주고 있으면 비교해서 높은 점수를 저장
-        Optional<GameRank> optionalGameRank = gameRankRepository.findById(userId);
-        if (!optionalGameRank.isPresent()) {
+        Optional<GameRank> optionalGameRank = gameRankRepository.findByUserId(userId);
+
+        if (optionalGameRank.isEmpty()) {
             GameRank newGameRank = new GameRank();
             newGameRank.setUserId(userId);
             newGameRank.setRound(round);
             newGameRank.setScore(newScore);
             gameRankRepository.save(newGameRank);
-            return true;
+            return ResponseEntity.ok("새 점수가 등록되었습니다(신규)");
         } else {
             GameRank existingGameRank = optionalGameRank.get();
             if (existingGameRank.getScore() < newScore) {
                 existingGameRank.newScore(newScore);
                 gameRankRepository.save(existingGameRank);
-                return true;
+                return ResponseEntity.ok("새 점수가 등록되었습니다(경신)");
             } else {
-                return false;
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("존재하지 않는 유저입니다.");
             }
         }
     }
