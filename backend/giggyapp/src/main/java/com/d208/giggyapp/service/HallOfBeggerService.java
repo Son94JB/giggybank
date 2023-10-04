d4b6842254dfe543db0d0aaccb027b234336d215
jaewook <jaewook71828@gmail.com>
2023-10-04 17:45:07 +0900

    Fix : 거지랭킹 수정중

--- backend/giggyapp/src/main/java/com/d208/giggyapp/service/HallOfBeggerService.java
+++ backend/giggyapp/src/main/java/com/d208/giggyapp/service/HallOfBeggerService.java
@@ -25,8 +25,8 @@ public class HallOfBeggerService {
     private final UserRepository userRepository;
 
     public ResponseEntity<?> getTopHallOfBegger() {
-//        String url = "https://j9d208.p.ssafy.io:8282/api/v1/rank/hall-of-begger";
-        String url = "http://127.0.0.1:8083/api/v1/rank/hall-of-begger";
+        String url = "https://j9d208.p.ssafy.io:8282/api/v1/rank/hall-of-begger";
+//        String url = "http://127.0.0.1:8083/api/v1/rank/hall-of-begger";
 
         RestTemplate restTemplate = new RestTemplate();
 
@@ -61,8 +61,8 @@ public class HallOfBeggerService {
     }
 
     public ResponseEntity<?> getHallOfBegger(UUID userId) {
-//        String url = "https://j9d208.p.ssafy.io:8282/api/v1/rank/hall-of-begger";
-        String url = "http://127.0.0.1:8083/api/v1/rank/hall-of-begger";
+        String url = "https://j9d208.p.ssafy.io:8282/api/v1/rank/hall-of-begger";
+//        String url = "http://127.0.0.1:8083/api/v1/rank/hall-of-begger";
 
         RestTemplate restTemplate = new RestTemplate();
 
@@ -77,15 +77,18 @@ public class HallOfBeggerService {
 
         HttpEntity<?> requestEntity = new HttpEntity<>(requestBody, headers);
         try {
+            System.out.println("33333333333333333333");
             ResponseEntity<List<BeggerNeighborDto>> response = restTemplate.exchange(
                     uri.toString(),
                     HttpMethod.POST,
                     requestEntity,
                     new ParameterizedTypeReference<List<BeggerNeighborDto>>() {}
                     );
+            System.out.println("11111111111111111111");
             List<BeggerNeighborDto> beggerNeighborDtos = response.getBody();
+            System.out.println("22222222222222222222");
             List<BeggerNeighborResultDto> beggerNeighborResultDtos = new ArrayList<>();
-            
+            System.out.println("444444444444444444444");
             for (BeggerNeighborDto beggerNeighborDto : beggerNeighborDtos){
                 User user = userRepository.findById(beggerNeighborDto.getUserId()).orElseThrow(() ->
                         new NoSuchElementException("존재하지 않는 유저입니다."));
