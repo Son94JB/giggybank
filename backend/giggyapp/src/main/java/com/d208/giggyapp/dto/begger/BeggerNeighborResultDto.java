917757873fab164b6d722f5d05f120d9bb84ad19
jaewook <jaewook71828@gmail.com>
2023-10-04 17:55:39 +0900

    Fix : 거지랭킹조회 수정

--- backend/giggyapp/src/main/java/com/d208/giggyapp/dto/begger/BeggerNeighborResultDto.java
+++ backend/giggyapp/src/main/java/com/d208/giggyapp/dto/begger/BeggerNeighborResultDto.java
@@ -1,10 +1,14 @@
 package com.d208.giggyapp.dto.begger;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class BeggerNeighborResultDto {
     private String nickName;
     private Long rank;
