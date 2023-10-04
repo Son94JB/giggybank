c30ee13d4b5845f8907318345c51d5ca3f8476e6
jaewook <jaewook71828@gmail.com>
2023-10-04 20:33:52 +0900

    Fix : 내 거지랭킹 조회 수정

--- backend/giggyrank/src/main/java/com/d208/giggyrank/service/HallOfBeggerService.java
+++ backend/giggyrank/src/main/java/com/d208/giggyrank/service/HallOfBeggerService.java
@@ -1,17 +1,14 @@
 package com.d208.giggyrank.service;
 
 import com.d208.giggyrank.dto.BeggerRankDto;
-import com.d208.giggyrank.dto.BeggerRankNeighborDto;
 import com.d208.giggyrank.dto.BeggerRankResultDto;
+import com.d208.giggyrank.dto.MyBeggerRankDto;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -51,33 +48,26 @@ public class HallOfBeggerService {
                 .collect(Collectors.toList());
     }
 
-    public List<BeggerRankNeighborDto> getUserRankAndNeighbors(BeggerRankDto beggerRankDto) {
+    public MyBeggerRankDto getUserRank(BeggerRankDto beggerRankDto) {
         Long rank = zSetOperations.reverseRank(rankingKey, beggerRankDto.getUserId().toString());
 
         if (rank == null) {
             throw new RuntimeException("Failed to get the rank of user: " + beggerRankDto.getUserId());
         }
 
-        // 유저 랭킹에서 위로 2명, 아래로 2명을 포함한 범위 계산
-        Long start = Math.max(0, rank - 2); // 시작 인덱스가 음수가 되지 않도록 보장
-        Long end = rank + 2;
+        Double score = zSetOperations.score(rankingKey, beggerRankDto.getUserId().toString());
 
-        // 해당 범위의 유저 ID와 점수(score)를 가져옴
-        Set<ZSetOperations.TypedTuple<String>> rangeWithScores =
-                zSetOperations.rangeWithScores(rankingKey, start, end);
-
-        // 결과 변환: User ID와 Rank, Score 정보 포함
-        List<BeggerRankNeighborDto> result = new ArrayList<>();
-
-        for (ZSetOperations.TypedTuple<String> tuple : rangeWithScores) {
-            result.add(BeggerRankNeighborDto.builder()
-                    .userId(UUID.fromString(tuple.getValue()))
-                    .rank(++start)
-                    .ratio(tuple.getScore())
-                    .build());
+        if (score == null) {
+            throw new RuntimeException("Failed to get the score of user: " + beggerRankDto.getUserId());
         }
 
-        return result;
+        MyBeggerRankDto myBeggerRankDto = MyBeggerRankDto.builder()
+                .rank(zSetOperations.size(rankingKey) - rank)
+                .ratio(score)
+                .build();
+
+        return myBeggerRankDto;
     }
+
 
 }
