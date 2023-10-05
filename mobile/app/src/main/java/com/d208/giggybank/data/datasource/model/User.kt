a821a421d25d374ec53c2bcc00521b949993aeed
김준성 <know0329@pusan.ac.kr>
2023-10-05 17:11:05 +0900

    Feat : 안드로이드 최종
    Former-commit-id: dcc25888cbe66202abf933e5d4c8b2a7140575c1

--- mobile/app/src/main/java/com/d208/giggybank/data/datasource/model/User.kt
+++
@@ -1,5 +0,0 @@
-package com.d208.giggybank.data.datasource.model
-
-import java.util.UUID
-
-data class User(var id : UUID  = UUID.randomUUID(), var email : String = "", var nickname : String = "", var targetAmount : Int = 0, val currentAmount : Int = 0, var funToken : String ="", var refreshToken : String = "", var letLife : Int =0)
