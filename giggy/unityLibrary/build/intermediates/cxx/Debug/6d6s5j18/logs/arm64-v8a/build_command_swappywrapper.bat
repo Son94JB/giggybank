125296e11cf597e445ce425bac0fb955b19aff7f
김준성 <know0329@pusan.ac.kr>
2023-10-05 12:04:12 +0900

    Feat : 유니티 빌드 파일 제거
    Former-commit-id: 97905219c65463a0a3eeb325346d82f0544b2bc7

--- giggy/unityLibrary/build/intermediates/cxx/Debug/6d6s5j18/logs/arm64-v8a/build_command_swappywrapper.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\SSAFY\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\SSAFY\\Documents\\S09P22D208\\giggy\\unityLibrary\\.cxx\\Debug\\6d6s5j18\\arm64-v8a" ^
-  swappywrapper
